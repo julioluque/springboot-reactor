69effff57adf8a926c0858cf762026b4fefbe916
julioluque <julio.a.luque@gmail.com>
2021-01-09 19:03:35 -0300

    Just, map, filter subscribe etc

--- src/main/java/com/jluque/springboot/reactor/app/SpringbootReactorApplication.java
+++ src/main/java/com/jluque/springboot/reactor/app/SpringbootReactorApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jluque.springboot.reactor.app.model.Usuario;
+
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
@@ -19,18 +21,28 @@ public class SpringbootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Julio", "Alfredo", "Luque", "Ticona", "aelfraed").doOnNext(e -> {
-			if (e.isEmpty()) {
-				throw new RuntimeException("No puede ser vacio");
-			}
-			System.out.println(e);
-		});
-		nombres.subscribe(e -> log.info(e), error -> log.error(error.getMessage()), new Runnable() {
+		Flux<String> nombres = Flux.just("Julio Luque", "Cecilia Ortiz", "Delfina Luque", "Delfina Ortiz",
+				"Alfredo Luque", "Julio Ticona", "Alfredo Ticona", "Griselda Ortiz");
+
+		Flux<Usuario> usuarios = nombres
+				.map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+				.filter(usuario -> usuario.getApellido().equalsIgnoreCase("ortiz")).doOnNext(usuario -> {
+					if (usuario == null) {
+						throw new RuntimeException("No puede ser vacio");
+					}
+					System.out.println(usuario.getNombre().concat(" ").concat(usuario.getApellido()));
+				}).map(usuario -> {
+					String nombre = usuario.getNombre().toLowerCase();
+					usuario.setNombre(nombre);
+					return usuario;
+				});
+
+		usuarios.subscribe(e -> log.info(e.toString()), error -> log.error(error.getMessage()), new Runnable() {
 			@Override
 			public void run() {
-				log.info("Flux finalizado correctamente......!!!");
+				log.info("Flux finalizado correctamente!!!");
 			}
-			
+
 		});
 	}
 
